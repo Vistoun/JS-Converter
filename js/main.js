0914702d45c2a4139ccb7eb6957fa94c637d3a20
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2019-09-29 16:07:07 +0200

    JS
    Some JS

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let number = document.getElementById('number');
+let result = document.getElementById('result');
+let button = document.getElementById('button');
+
+
+/* 1 USD = 23.61777 CZK, 
+   1 EUR = 25.840273 CZK, 
+   1 PLN = 5.89558584 CZK, 
+*/
+
+function Converter(vstup){
+    
+    usd = 23.61777;
+    return (vstup / usd);
+   
+}
+
+button.addEventListener('click', function(){
+   
+    result.innerText = Converter(number.value);
+    
+
+})
