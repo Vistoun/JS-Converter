d8f2fc7f8d86edc280fc9068910802895a190672
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2019-10-01 18:51:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,17 +10,20 @@ let currency = document.getElementById('converter');
 
 
 function Converter(currency, input) {
+
     switch (currency) {
 
         case "USD":
+        
             usd = 23.61777;
             return (input / usd).toFixed(2);
-
         case "EUR":
+
             eur = 25.840273;
             return (input / eur).toFixed(2);
 
         case "PLN":
+
             pln = 5.89558584;
             return (input / pln).toFixed(2);
     }
