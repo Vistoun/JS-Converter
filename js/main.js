d848604548396302cbacfc1c0957637bd68238d7
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2019-10-02 18:19:48 +0200

    JS

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let result = document.getElementById('result');
 let currency = document.getElementById('converter');
 
 
+
 /* 1 USD = 23.61777 CZK, 
    1 EUR = 25.840273 CZK, 
    1 PLN = 5.89558584 CZK, 
@@ -11,27 +12,37 @@ let currency = document.getElementById('converter');
 
 function Converter(currency, input) {
 
+    if (isNaN(input)) {
+        number.value = null;
+        alert("Nazadal jsi cislo")
+    }
     switch (currency) {
 
         case "USD":
 
             usd = 23.61777;
-            return (input / usd).toFixed(2);
+            return (input / usd).toFixed(2) + " $";
         case "EUR":
 
             eur = 25.840273;
-            return (input / eur).toFixed(2);
+            return (input / eur).toFixed(2) + " €";
 
         case "PLN":
 
             pln = 5.89558584;
-            return (input / pln).toFixed(2);
+            return (input / pln).toFixed(2) + " zł";
     }
 }
 
 
 number.addEventListener('input', function () {
 
-    result.innerHTML = Converter(currency.value, number.value);
+    result.value = Converter(currency.value, number.value);
 
 })
+currency.addEventListener('change', function () {
+
+    result.value = Converter(currency.value, number.value);
+
+})
+
